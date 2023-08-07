a92c8e352cd73ab7337888302a6aa240a5de6425
Karol Szymański <karol2134@op.pl>
2023-08-07 14:32:50 +0200

    Change path in RepositoryRestResource part2

--- src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
+++ src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
@@ -3,7 +3,7 @@ package com.vacation.com.vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "vacations")
+@RepositoryRestResource(path = "vacations", collectionResourceRel = "vacations")
 interface HolidayLeaveRepository extends JpaRepository<HolidayLeave, Integer>{
 
 }
