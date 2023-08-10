e167258558df8b3ef0e5035dadf5c25d5e890777
Karol Szymański <karol2134@op.pl>
2023-08-10 15:42:22 +0200

    Add script.js

---
+++ src/FrontEnd/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const vacationTable = document.getElementById("vacationTable").getElementsByTagName('tbody')[0];
+
+    // Pobierz dane z serwera (zakładamy, że serwer działa na http://localhost:8080)
+    fetch("http://localhost:8080/sampledata")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(vacation => {
+                const row = vacationTable.insertRow();
+                row.innerHTML = `
+          <td>${vacation.id}</td>
+          <td>${vacation.description}</td>
+          <td>${vacation.daysNum}</td>
+          <td>${vacation.done}</td>
+        `;
+            });
+        })
+        .catch(error => console.error("Error fetching data:", error));
+});
