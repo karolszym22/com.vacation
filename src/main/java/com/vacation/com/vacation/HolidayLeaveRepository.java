735b358a9b9a9216b49ac3fbfd856f50f7a53227
Karol Szymański <karol2134@op.pl>
2023-08-13 15:56:10 +0200

    Refactoring HolidayLeaveRepository.java to create class SqlHolidayLeaveRepository

--- src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
+++ src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
@@ -1,15 +1,18 @@
 package com.vacation.com.vacation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface HolidayLeaveRepository extends JpaRepository<HolidayLeave, Integer>{
-    @RestResource(path = "done", rel = "done")
-    List<HolidayLeave> findByDone(@Param("state") boolean done);
+public interface HolidayLeaveRepository {
+    List<HolidayLeave> findAll();
+
+    Page<HolidayLeave> findAll(Pageable page);
+    Optional<HolidayLeave> findById(Integer id);
+
+    boolean existsById(Integer id);
+    HolidayLeave save(HolidayLeave entity);
+
 }
