b2baf5c42a8755e98deb9161cfe2187dcb930df5
Karol Szymański <karol2134@op.pl>
2023-08-13 15:45:48 +0200

    Adding CorsConfig.java and application.properties modification to solve the cors security problem

---
+++ src/main/java/com/vacation/com/vacation/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.vacation.com.vacation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500/")  // Dodaj tutaj odpowiedni adres swojej aplikacji frontendowej
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
