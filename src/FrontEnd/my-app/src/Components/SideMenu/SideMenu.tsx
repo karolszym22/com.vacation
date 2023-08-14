3e3bfef0608632a90c2362564736f7afea658939
Karol Szymański <karol2134@op.pl>
2023-08-14 16:53:20 +0200

    Creating SideMenu.tsx component

--- src/FrontEnd/my-app/src/Components/SideMenu/SideMenu.tsx
+++ src/FrontEnd/my-app/src/Components/SideMenu/SideMenu.tsx
@@ -1,50 +1,103 @@
-import React, { useState, useEffect, useContext } from 'react';
-import styled from 'styled-components'
-import { NavLink } from "react-router-dom";
+import React from "react";
+import styled from "styled-components";
+import { Home, List } from "feather-icons-react";
 
-const MenuNav = styled.p`
-color: #00cdd1;
-font-size: 24px;
-text-decoration: none; 
-cursor: pointer;
-&:hover {
-  color: #b2dbe6;
-}
+const CustomHomeIcon = styled(Home)`
+  width: 15px;
+  height: 15px;
+  margin: 5px;
+`;
+const CustomListIcon = styled(List)`
+  width: 15px;
+  height: 15px;
+  margin: 5px;
 `;
 
-const SideMenu = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    @media (max-width: 568px) {
-    display: none ;
+
+const MenuLogoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center; /* Added alignment */
+  padding: 10px 20px; /* Added padding */
+  box-sizing: border-box;
+`;
+
+const MenuNavLogo = styled.h1`
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    color: #b2dbe6;
   }
 `;
-const MenuContainer = styled.div`
-    width: 60%;
-    height: 50px;
-    display: flex ;
-    justify-content: space-around;
-    align-items: center;
-    
+
+const NavLinkName = styled.a`
+  font-size: 14px;
+  margin: 5px;
+`;
+const MenuNavLink = styled.div`
+  color: white;
+  padding: 8px 5px;
+  display: flex;
+  align-items: center; /* Added alignment */
+  cursor: pointer;
+  &:hover {
+    background-color: #293744;
+  }
 `;
 
-const Menu = () =>
-{
+const SubMenuContainer = styled.div<{ expanded: boolean }>`
+  display: ${(props) => (props.expanded ? "flex" : "none")};
+  background-color: #293744;
+  padding-left: 20px;
+  font-size: 10px;
+  flex-direction: column;
+  color: #d8d3d3;
+`;
 
-  
-    return(
-     <SideMenu >
-       <MenuContainer>
-           
-          <MenuNav as={NavLink} to="/">Interfejs</MenuNav>
-          <MenuNav as={NavLink} to="/szukaj">Wikipedia</MenuNav>
-          <MenuNav as={NavLink} to="/notes">Notatki</MenuNav>
-          <MenuNav as={NavLink} to="/help">Pomoc</MenuNav>
-          
-      </MenuContainer>
-    </SideMenu >)
+const SubMenu: React.FC<{ expanded: boolean }> = ({ expanded }) => (
+  <SubMenuContainer expanded={expanded}>
+    <NavLinkName>Urlopy Wypoczynkowe</NavLinkName>
+    <NavLinkName>Urlopy Macierzyńskie</NavLinkName>
+    <NavLinkName>Urlopy Tacierzyńskie</NavLinkName>
+  </SubMenuContainer>
+);
 
+const SideMenu = styled.div`
+  width: 280px;
+  background-color: #2e4051;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-direction: column; /* Changed to column */
+`;
 
-}
-    
+const Menu = () => {
+  const [isSubMenuOpen, setSubMenuOpen] = React.useState(false);
+
+  const toggleSubMenu = () => {
+    setSubMenuOpen(!isSubMenuOpen);
+  };
+
+  return (
+    <SideMenu>
+      <MenuLogoContainer>
+        <MenuNavLogo>Urlopy</MenuNavLogo>
+      </MenuLogoContainer>
+      <MenuNavLink>
+        <CustomHomeIcon />
+        <NavLinkName>Strona główna</NavLinkName>
+      </MenuNavLink>
+      <MenuNavLink onClick={toggleSubMenu}>
+        <CustomListIcon />
+        <NavLinkName>Lista urlopów</NavLinkName>
+      </MenuNavLink>
+      <SubMenu expanded={isSubMenuOpen} />
+    </SideMenu>
+  );
+};
+
+export default Menu;
