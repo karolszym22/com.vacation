2b94e41bc36c85a511bf9825054bc8c518c0c6a4
Karol Szymański <karol2134@op.pl>
2023-08-15 21:18:59 +0200

    Adding new component - Header.tsx

---
+++ src/FrontEnd/my-app/src/Components/Header/Header.tsx
@@ -0,0 +1,126 @@
+import styled from "styled-components";
+import { User } from "feather-icons-react";
+import img from "../../resources/pexels-karolina-grabowska-7876708.jpg";
+import userIcon from "../../resources/user.png";
+
+const UserIcon = styled.div`
+  background-image: url(${userIcon});
+  color: white;
+  height: 24px;
+  width: 24px;
+  margin: 5px 20px;
+  background-color: white;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 500px;
+  position: relative; /* Dodaj pozycję względem której będzie pozycjonowany pseudoelement */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${img});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center 60%;
+    opacity: 0.2;
+    z-index: -1;
+  }
+`;
+
+const HeaderTop = styled.div`
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  position: absolute;
+  top: 0px;
+  display: flex;
+  justify-content: end;
+`;
+const HeaderTitle = styled.h1`
+  width: 100%;
+  color: #928d8d;
+  text-align: center;
+`;
+const HeaderBackground = styled.div`
+  width: 70%;
+  height: 70%;
+  margin-right: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const HeaderElement = styled.div`
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Value = styled.div`
+  font-size: 20px;
+  color: #565454;
+  font-weight: bold;
+`;
+const HeaderElementValue = styled.div`
+  width: 120px;
+  height: 120px;
+  border-radius: 150px;
+  font-size: 34px;
+  font-weight: bold;
+  border: 15px solid ${({ color }) => color};
+  background-color: rgba(255, 255, 255, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 50;
+`;
+
+const HeaderElementContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTop>
+        <UserIcon></UserIcon>
+      </HeaderTop>
+      <HeaderBackground>
+        <HeaderTitle>Dodawaj i obserwuj swoje urlopy! </HeaderTitle>
+        <HeaderElementContainer>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="#2cfc03">10</HeaderElementValue>
+            <Value>Zaakceptowane</Value>
+          </HeaderElement>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="orange">23</HeaderElementValue>
+            <Value>W trakcie</Value>
+          </HeaderElement>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="brown">0</HeaderElementValue>
+            <Value>Odrzucone</Value>
+          </HeaderElement>
+        </HeaderElementContainer>
+      </HeaderBackground>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
