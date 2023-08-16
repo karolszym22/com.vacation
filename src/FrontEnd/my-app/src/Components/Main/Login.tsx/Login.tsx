da1cadf2d8ccb011d871fd98f7112a496af3cf9a
Karol Szymański <karol2134@op.pl>
2023-08-16 13:31:07 +0200

    Adding new component to login - Login.tsx

---
+++ src/FrontEnd/my-app/src/Components/Main/Login.tsx/Login.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+
+const Login: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/users/login', {
+                username,
+                password,
+            });
+            console.log(response.data); // Handle successful login
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
