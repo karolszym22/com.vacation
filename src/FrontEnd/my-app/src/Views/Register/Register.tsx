01401e4c44bee48676709644af1ac02d7535c163
Karol Szymański <karol2134@op.pl>
2023-08-21 22:13:07 +0200

    Adding styled components end errors validation system

--- src/FrontEnd/my-app/src/Views/Register/Register.tsx
+++ src/FrontEnd/my-app/src/Views/Register/Register.tsx
@@ -1,30 +1,76 @@
 import React, { useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 230px;
+`;
+const Title = styled.h2`
+  font-size: 1.5em;
+  margin-bottom: 20px;
+`;
+
+const ErrorText = styled.div`
+  color: red;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin: 5px 0;
+  width: 100%;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin: 5px 0;
+  width: 30%;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
 
 const Register: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [employerType, setEmployerType] = useState("");
   const [email, setEmail] = useState("");
-  const [error, setError] = useState(""); 
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [emailError, setEmailError] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (username.length < 3) {
-      setError("Username must be at least 3 characters long.");
+      setUsernameError("Username must be at least 3 characters long.");
       return;
     }
 
     if (password.length < 8) {
-      setError("Password must be at least 8 characters long.");
+      setPasswordError("Password must be at least 8 characters long.");
       return;
     }
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!email.match(emailPattern)) {
-      setError("Invalid email address.");
+      setEmailError("Invalid email address.");
       return;
     }
 
@@ -39,43 +85,47 @@ const Register: React.FC = () => {
     } catch (error) {
       console.error("Registration error:", error);
     }
+    
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div style={{ color: "red" }}>{error}</div>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <select
-          value={employerType}
-          onChange={(e) => setEmployerType(e.target.value)}
-        >
-          <option value="">Select user type</option>
-          <option value="HR">HR</option>
-          <option value="Pracownik">Pracownik</option>
-          <option value="Pracodawca">Pracodawca</option>
-        </select>
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <Container>
+    <Title>Register</Title>
+    <Form onSubmit={handleSubmit}>
+      
+      <Input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <ErrorText>{usernameError}</ErrorText>
+      <Input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <ErrorText>{usernameError}</ErrorText>
+      <Input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <ErrorText>{usernameError}</ErrorText>
+      <Select
+        value={employerType}
+        onChange={(e) => setEmployerType(e.target.value)}
+      >
+        <option value="">Select user type</option>
+        <option value="HR">HR</option>
+        <option value="Pracownik">Pracownik</option>
+        <option value="Pracodawca">Pracodawca</option>
+      </Select>
+      <Button type="submit">Register</Button>
+    </Form>
+  </Container>
   );
 };
 
