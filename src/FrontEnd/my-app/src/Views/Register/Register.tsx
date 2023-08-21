179936c43b0885a2ab1e255afb095baf68f53d46
Karol Szymański <karol2134@op.pl>
2023-08-21 14:35:35 +0200

    Modifying the Register.tsx component
    - adding new states: employerType and email

--- src/FrontEnd/my-app/src/Views/Register/Register.tsx
+++ src/FrontEnd/my-app/src/Views/Register/Register.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import SignIn from '../SignIn/SigIn'
-
+import { NavLink } from 'react-router-dom';
 
 const Register: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [employerType, setEmployerType] = useState('');
+    const [email, setEmail] = useState(''); 
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -13,10 +14,12 @@ const Register: React.FC = () => {
             const response = await axios.post('http://localhost:8080/NewUser', {
                 username,
                 password,
+                employerType,
+                email, 
             });
-            console.log(response.data); 
+            console.log(response.data);
         } catch (error) {
-            console.error('Login error:', error);
+            console.error('Registration error:', error);
         }
     };
 
@@ -36,9 +39,23 @@ const Register: React.FC = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button type="submit">Login</button>
+                <input
+                    type="email" // Typ pola email
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <select
+                    value={employerType}
+                    onChange={(e) => setEmployerType(e.target.value)}
+                >
+                    <option value="">Select user type</option>
+                    <option value="HR">HR</option>
+                    <option value="Pracownik">Pracownik</option>
+                    <option value="Pracodawca">Pracodawca</option>
+                </select>
+                <button type="submit">Register</button>
             </form>
-            <h3></h3>
         </div>
     );
 };
