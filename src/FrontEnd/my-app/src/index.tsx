bb306df3e8870639ab5c7b11798d1d63a7637e6c
Karol Szymański <karol2134@op.pl>
2023-08-27 22:30:06 +0200

    Adding new route for VacationPreview

--- src/FrontEnd/my-app/src/index.tsx
+++ src/FrontEnd/my-app/src/index.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
+import SignIn from "./Views/SignIn/SigIn"
+import Register from './Views/Register/Register';
+import { Provider } from 'react-redux'
+import store from './Reducers/Store/index'
+import NewVacation from './Views/NewVacation/NewVacation';
+import VacationPreview from './Components/VacationPreview/VacationPreview';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <BrowserRouter>
+    
+      <Routes>
+      <Route path="/" element={<App />} />
+              <Route path="/SignIn" element={<SignIn/>}/>
+              <Route path="/Register" element={<Register/>}/>
+              <Route path="/NewVacation" element={<NewVacation/>}/>
+              <Route path="/vacationId/:paramValue" element={<VacationPreview />} />
+
+      </Routes>
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
