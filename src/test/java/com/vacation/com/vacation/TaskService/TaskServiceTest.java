308697a847fc74760dc8f3ad6e7f1e1d7d50cae0
Karol Szymański <karol2134@op.pl>
2023-09-01 10:45:55 +0200

    Creating a test to check whether the appropriate steps are generated for a particular type of employee

--- src/test/java/com/vacation/com/vacation/TaskService/TaskServiceTest.java
+++ src/test/java/com/vacation/com/vacation/TaskService/TaskServiceTest.java
@@ -1,49 +1,69 @@
-package com.vacation.com.vacation.TaskService;
-import com.vacation.com.vacation.Service.TaskService;
+package com.vacation.com.vacation.Service;
+
+import com.vacation.com.vacation.Model.TaskData;
+import com.vacation.com.vacation.Model.Task.Tasks;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-
-@ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
 
-    @InjectMocks
+    @Mock
     private TaskService taskService;
 
-    @Test
-    public void testProcessTaskPracownikZakceptuj() {
-        String result = taskService.processTask("Zakceptuj", "PRACOWNIK");
-        assertEquals("Sent 'Zakceptuj' task.", result);
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testProcessTaskHRZakceptuj() {
-        String result = taskService.processTask("Zakceptuj", "HR");
-        assertEquals("Sent 'Zakceptuj' task.", result);
+    public void testProcessTaskForHRAndAccepted() {
+        String taskStatus = "Pracodawca:Zaakceptowane";
+        String userType = "HR";
+
+        TaskData expectedTaskData = new TaskData();
+        expectedTaskData.setTaskEnums(Arrays.asList(Tasks.ODRZUC, Tasks.ZWROC, Tasks.ZAAKCEPTUJ));
+
+        when(taskService.processTask(taskStatus, userType)).thenReturn(expectedTaskData);
+
+        TaskData actualTaskData = taskService.processTask(taskStatus, userType);
+
+        assertEquals(expectedTaskData.getTaskEnums(), actualTaskData.getTaskEnums());
     }
 
     @Test
-    public void testProcessTaskHROdrzuc() {
-        String result = taskService.processTask("Odrzuc", "HL");
-        assertEquals("Sent 'Odrzuc' task.", result);
+    public void testProcessTaskForPracodawcaAndDodane() {
+        String taskStatus = "Pracownik:Dodane";
+        String userType = "PRACODAWCA";
+
+        TaskData expectedTaskData = new TaskData();
+        expectedTaskData.setTaskEnums(Arrays.asList(Tasks.ODRZUC, Tasks.ZWROC, Tasks.ZAAKCEPTUJ));
+
+        when(taskService.processTask(taskStatus, userType)).thenReturn(expectedTaskData);
+
+        TaskData actualTaskData = taskService.processTask(taskStatus, userType);
+
+        assertEquals(expectedTaskData.getTaskEnums(), actualTaskData.getTaskEnums());
     }
 
     @Test
-    public void testProcessTaskPracodawcaZwroc() {
-        String result = taskService.processTask("Zwroc", "PRACODAWCA");
-        assertEquals("Sent 'ODRZUC', 'Zwroc', 'Zakceptuj' tasks.", result);
-    }
+    public void testProcessTaskForPracodawcaAndZwroc() {
+        String taskStatus = "HR:Zwróc";
+        String userType = "PRACODAWCA";
 
+        TaskData expectedTaskData = new TaskData();
+        expectedTaskData.setTaskEnums(Arrays.asList(Tasks.ODRZUC, Tasks.ZWROC, Tasks.ZAAKCEPTUJ));
 
-    @Test
-    public void testProcessTaskNoMatchingTasks() {
-        String result = taskService.processTask("InnyStatus", "InnyUserType");
-        assertEquals("No matching tasks sent.", result);
+        when(taskService.processTask(taskStatus, userType)).thenReturn(expectedTaskData);
+
+        TaskData actualTaskData = taskService.processTask(taskStatus, userType);
+
+        assertEquals(expectedTaskData.getTaskEnums(), actualTaskData.getTaskEnums());
     }
 }
