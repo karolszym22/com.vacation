a5a1d4d1f7d5dbf1a70459afcab8df1af4e22d17
Karol Szymański <karol2134@op.pl>
2023-09-02 22:09:15 +0200

    Adding new method for HolidayLeaveRepository's interface: findByPersonId

--- src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
+++ src/main/java/com/vacation/com/vacation/HolidayLeaveRepository.java
@@ -3,13 +3,15 @@ package com.vacation.com.vacation;
 import com.vacation.com.vacation.Model.HolidayLeave;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface HolidayLeaveRepository {
     List<HolidayLeave> findAll();
-
+    List<HolidayLeave> findByPersonId(Integer personId);
     Page<HolidayLeave> findAll(Pageable page);
     Optional<HolidayLeave> findById(Integer id);
     void deleteById(Integer id);
