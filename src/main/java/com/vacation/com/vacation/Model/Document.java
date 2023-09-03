0d283910d7f5f7aff283cab935038add0f313379
Karol Szymański <karol2134@op.pl>
2023-09-03 21:23:49 +0200

    Creating new model: Document

---
+++ src/main/java/com/vacation/com/vacation/Model/Document.java
@@ -0,0 +1,70 @@
+package com.vacation.com.vacation.Model;
+
+import java.util.Date;
+
+public class Document {
+    private int id;
+    private Date currentData;
+    private String description;
+    private String employerName;
+    private int daysNum;
+    private String taskStatus;
+    private int vacationId;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCurrentData() {
+        return currentData;
+    }
+
+    public void setCurrentData(Date currentData) {
+        this.currentData = currentData;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmployerName() {
+        return employerName;
+    }
+
+    public void setEmployerName(String employerName) {
+        this.employerName = employerName;
+    }
+
+    public int getDaysNum() {
+        return daysNum;
+    }
+
+    public void setDaysNum(int daysNum) {
+        this.daysNum = daysNum;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public long getVacationId() {
+        return vacationId;
+    }
+
+    public void setVacationId(int vacationId) {
+        this.vacationId = vacationId;
+    }
+}
