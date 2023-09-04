77a35f9edcf4a08ceefca90e040fc70e1fa431ea
Karol Szymański <karol2134@op.pl>
2023-09-04 14:07:31 +0200

    Document Controller modification

--- src/main/java/com/vacation/com/vacation/Controller/DocumentController.java
+++ src/main/java/com/vacation/com/vacation/Controller/DocumentController.java
@@ -1,7 +1,7 @@
 package com.vacation.com.vacation.Controller;
 
 import com.vacation.com.vacation.Model.Document;
-import com.vacation.com.vacation.Repository.DocumentRepository;
+import com.vacation.com.vacation.Service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,33 +10,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/documents")
+@RequestMapping("/document/word")
 public class DocumentController {
 
-    private final DocumentRepository documentRepository;
+    private final DocumentService documentService;
 
     @Autowired
-    public DocumentController(DocumentRepository documentRepository) {
-        this.documentRepository = documentRepository;
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
     }
-
 
     @PostMapping
     public ResponseEntity<Document> createDocument(@RequestBody Document document) {
-        Document savedDocument = documentRepository.save(document);
+        Document savedDocument = documentService.createDocument(document);
         return new ResponseEntity<>(savedDocument, HttpStatus.CREATED);
     }
 
-
     @GetMapping
     public ResponseEntity<List<Document>> getAllDocuments() {
-        List<Document> documents = documentRepository.findAll();
+        List<Document> documents = documentService.getAllDocuments();
         if (!documents.isEmpty()) {
             return new ResponseEntity<>(documents, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-
 }
