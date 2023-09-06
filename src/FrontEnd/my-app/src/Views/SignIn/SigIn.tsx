c68e98871b75ca2927fa649677bbaa9f0ee9e61b
Karol Szymański <karol2134@op.pl>
2023-09-06 19:29:42 +0200

    Modification of the visual layer of the SigIn component

--- src/FrontEnd/my-app/src/Views/SignIn/SigIn.tsx
+++ src/FrontEnd/my-app/src/Views/SignIn/SigIn.tsx
@@ -4,6 +4,53 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { user } from "../../Components/Actions/actions";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin: 5px 0;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  padding: 10px 25px;
+  margin: 15px 80px;
+  width: 100px;
+  background-color: orange;
+  color: white;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5em;
+  margin-bottom: 20px;
+  color: #646262;
+`;
+const BottomTitle =  styled.h4`
+   color: black;
+   cursor: pointer;
+   &:hover {
+    color: #777575; 
+    
+  }
+`
+
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -35,24 +82,25 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
+    <Container>
+      <Title>Login</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
           type="text"
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
+        <Input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        <Button type="submit">Login</Button>
+      </Form>
+      <BottomTitle>Nie masz konta? Dołącz do nas jeszcze dzis!</BottomTitle>
+    </Container>
   );
 };
 
