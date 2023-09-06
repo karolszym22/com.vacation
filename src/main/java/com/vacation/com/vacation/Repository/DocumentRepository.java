9fa8ccdfb71d3109d4b749351eeb5385a2d4a7a3
Karol Szymański <karol2134@op.pl>
2023-09-06 10:46:24 +0200

    Adding new method to DocumentRepository a method that return a document by a specific personId and vacationId

--- src/main/java/com/vacation/com/vacation/Repository/DocumentRepository.java
+++ src/main/java/com/vacation/com/vacation/Repository/DocumentRepository.java
@@ -1,15 +1,19 @@
 package com.vacation.com.vacation.Repository;
 import com.vacation.com.vacation.Model.Document;
+import com.vacation.com.vacation.Model.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.List;
 
-public interface DocumentRepository {
-    List<Document> findAll();
-    List<Document> findByVacationId(Integer vacationId);
-    Page<Document> findAll(Pageable page);
-    Document save(Document entity);
+public interface DocumentRepository  extends JpaRepository<Document, Integer> {
 
+    Document findByVacationId(Integer vacationId);
+    Document save(Document entity);
+    Document  findById(int id);
+
+    Document findByVacationId(int vacationId);
+    Document findByPersonIdAndVacationId(int personId, int vacationId);
 }
