28bae1031d5352d3e8a3860598b5b9d0dd9d054c
Karol Szymański <karol2134@op.pl>
2023-09-21 23:11:06 +0200

    Refactoring: adding given, when and then comments

--- src/main/java/com/vacation/com/vacation/Controller/TaskController.java
+++ src/main/java/com/vacation/com/vacation/Controller/TaskController.java
@@ -1,4 +1,5 @@
 package com.vacation.com.vacation.Controller;
+
 import com.vacation.com.vacation.Model.TaskData;
 import com.vacation.com.vacation.Service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,15 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-
 @RestController
-@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
+
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
+
     @PostMapping("/tasksToDo")
     public ResponseEntity<TaskData> processTask(@RequestBody TaskData taskData) {
         TaskData result = taskService.processTask(taskData.getTaskStatus(), taskData.getUserType());
