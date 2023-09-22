f4dd8d4c8d366f9e0c3f0b8d8e6b8071fc8f65a8
Karol Szymański <karol2134@op.pl>
2023-09-22 21:56:37 +0200

    Small changes in vacationsReducer.tsx

--- src/FrontEnd/my-app/src/Reducers/vacationsReducer.tsx
+++ src/FrontEnd/my-app/src/Reducers/vacationsReducer.tsx
@@ -1,30 +1,41 @@
-interface User {
-    id: number;
-    name: string;
-    email: string;
-    employerType: string;
+
+
+export interface Vacation {
+  id?: number;
+  description?: string;
+  daysNum?: number;
+  done?: boolean;
+  taskStatus: string;
+  startDate?: string;
+  endDate?: string;
+  employerName: string;
+  personId: number;
+}
+
+interface State {
+  vacations: Vacation[];
+  vacationsCount: number;
+}
+
+type Action =
+  | { type: "VACATIONS_LIST"; payload: Vacation[] }
+  | { type: "SET_VACATIONS_COUNT"; payload: number };
+
+const initialState: State = {
+  vacations: [],
+  vacationsCount: 0,
+};
+
+const vacationReducer = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case "VACATIONS_LIST":
+      return { ...state, vacations: action.payload, vacationsCount: action.payload.length };
+    case "SET_VACATIONS_COUNT":
+      return { ...state, vacationsCount: action.payload };
+    default:
+      return state;
   }
-  
-  interface AppState {
-    user: User; 
-  }
-  
-  type AppAction =
-     { type: "EMPLOYEE_AUTHORIZATION"; payload: { user: User } }
-  
-  export const initialState: AppState = {
-    user: { id: 0, name: '', email: '', employerType: '' },
-  };
-  
-  const authorizationReducer = (state: AppState = initialState, action: AppAction): AppState => {
-    switch (action.type) {
-      case 'EMPLOYEE_AUTHORIZATION':
-  
-        return { ...state, user: action.payload.user};
-  
-      default:
-        return state;
-    }
-  };
-  
-  export default authorizationReducer;
+};
+
+export default vacationReducer;
+
