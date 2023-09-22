12b1aced73610ca9edceddcb520cc647bd9c3141
Karol Szymański <karol2134@op.pl>
2023-09-22 11:52:29 +0200

    Deleting unnecessary method

--- src/main/java/com/vacation/com/vacation/Service/UserService.java
+++ src/main/java/com/vacation/com/vacation/Service/UserService.java
@@ -2,36 +2,15 @@ package com.vacation.com.vacation.Service;
 
 import com.vacation.com.vacation.Model.UserEntity;
 import com.vacation.com.vacation.Repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService  {
 
     private final UserRepository userRepository;
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException("Użytkownik nie znaleziony: " + email);
-        }
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
-        return new User(
-                user.getEmail(),
-                user.getPassword(),
-                authorities
-        );
-    }
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
