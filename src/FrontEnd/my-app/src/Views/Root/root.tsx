9ba250777505396ab983b24ee85f9979af78d808
Karol Szymański <karol2134@op.pl>
2023-09-30 14:46:45 +0200

    Adding new component: root.tsx: -this component is now the main component that handles all routes, context and redux actions using Provider

---
+++ src/FrontEnd/my-app/src/Views/Root/root.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "../../Views/SignIn/SigIn";
+import "../../index.css"
+import Register from "../../Views/Register/Register";
+import { Provider, useDispatch } from "react-redux";
+import store from "../../Reducers/Store/index";
+import NewVacation from "../../Views/NewVacation/NewVacation";
+import VacationPreview from "../../Components/VacationPreview/VacationPreview";
+import MainTemplate from "../MainTemplate/MainTemplate";
+import Main from "../MainSite/Main";
+import OverlayVisibleProvider from "../../Components/Context/OverlayVisibleContext";
+import { user } from "../../Components/Actions/actions";
+
+const Root = () => {
+
+
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <OverlayVisibleProvider>
+          <MainTemplate>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/signIn" element={<SignIn />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/newVacation" element={<NewVacation />} />
+              <Route
+                path="/vacationId/:paramValue"
+                element={<VacationPreview />}
+              />
+            </Routes>
+          </MainTemplate>
+        </OverlayVisibleProvider>
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default Root;
