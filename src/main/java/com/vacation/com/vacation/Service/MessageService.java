4c247b8a81bb7680d56b4f2d24301281a6a619f2
Karol Szymański <karol2134@op.pl>
2023-10-07 23:46:58 +0200

    MessageService modification:
    adding new methods: getAllVacations, addingMessage, getCorrespondenceMessages

--- src/main/java/com/vacation/com/vacation/Service/MessageService.java
+++ src/main/java/com/vacation/com/vacation/Service/MessageService.java
@@ -1,16 +1,31 @@
 package com.vacation.com.vacation.Service;
+
 import com.vacation.com.vacation.Model.HolidayLeave;
 import com.vacation.com.vacation.Model.Message;
+import com.vacation.com.vacation.Model.UserEntity;
 import com.vacation.com.vacation.Repository.MessageRepository;
+
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class MessageService {
+public class MessageService  {
+
     private final MessageRepository messageRepository;
+
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
-    public void createVacation(Message message) {
-        messageRepository.save(message);
+    public void addingMessage(Message message) {
+         messageRepository.save(message);
+    };
+    public List<Message> getAllVacations() {;
+        return messageRepository.findAll();
     }
+
+    public List<Message> getCorrespondenceMessages(int corespondenceId) {
+        return messageRepository.findByCorrespondenceId(corespondenceId);
+    }
+
 }
