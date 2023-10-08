2ab34792a5e31a068308f6292411e8d57cd5ad19
Karol Szymański <karol2134@op.pl>
2023-10-08 14:50:35 +0200

    Adding new fields and getters&setters in Message

--- src/main/java/com/vacation/com/vacation/Model/Message.java
+++ src/main/java/com/vacation/com/vacation/Model/Message.java
@@ -4,16 +4,83 @@ import jakarta.persistence.*;
 
 import java.util.Date;
 
+@Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    private int correspondenceId;
     @Temporal(TemporalType.DATE)
     private Date date;
     private String title;
+
+    public int getCorrespondenceId() {
+        return correspondenceId;
+    }
+
+    public void setCorrespondenceId(int correspondenceId) {
+        this.correspondenceId = correspondenceId;
+    }
+
     private String description;
     private int authorId;
-    private int personId;
+    private String authorName;
 
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    private int recipientId;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(int recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
