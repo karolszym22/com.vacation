76344c4bc06419596afd8062943901c2e0f30dd7
Karol Szymański <karol2134@op.pl>
2023-10-17 20:38:45 +0200

    creating new hook: useVacationsHooks

---
+++ src/FrontEnd/my-app/src/Hooks/useVacationsHooks.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+interface Vacation {
+  id: number;
+  description: string;
+  daysNum: number;
+  done: boolean;
+  taskStatus: string;
+}
+
+export const useVacations = () => {
+  const [vacations, setVacations] = useState<Vacation[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/vacations")
+      .then((response) => response.json())
+      .then((data) => {
+        setVacations(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return { vacations, setVacations };
+};
+
+export const usePreviewClick = () => {
+  const handlePreviewClick = (vacationId: number) => {
+    fetch(`http://localhost:8080/vacationsPreview`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ vacationId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {});
+  };
+
+  return handlePreviewClick;
+};
