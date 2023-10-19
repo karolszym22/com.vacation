72a805c63c9243c44d8675afa0f070388ca564f9
Karol Szymański <karol2134@op.pl>
2023-10-19 15:11:51 +0200

    creating new hook for Register.tsx: useHandleRegister.ts

---
+++ src/FrontEnd/my-app/src/Hooks/useHandleRegister.ts
@@ -0,0 +1,59 @@
+import { useState, FormEvent } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const useHandleRegister = () => {
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: FormEvent, username: string, password: string, confirmPassword: string, email: string, employerType: string) => {
+    e.preventDefault(); 
+    if (username.length < 3) {
+      setUsernameError('Nazwa użytkownika musi mieć co najmniej 3 znaki.');
+      return;
+    }
+
+    if (password.length < 8) {
+      setPasswordError('Hasło musi mieć co najmniej 8 znaków.');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setConfirmPasswordError('Hasła nie pasują do siebie.');
+      return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!email.match(emailPattern)) {
+      setEmailError('Nieprawidłowy adres email.');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8080/newUser', {
+        username,
+        password,
+        employerType,
+        email,
+      });
+
+      navigate('/signIn');
+    } catch (error) {
+      console.error('Błąd rejestracji:', error);
+    }
+  };
+
+  return {
+    handleSubmit,
+    usernameError,
+    passwordError,
+    confirmPasswordError,
+    emailError,
+  };
+};
+
+export default useHandleRegister;
