9fafed7cb4964865d0c62d42c011c04d19e1173d
Karol Szymański <karol2134@op.pl>
2023-10-31 22:02:56 +0100

    -Adding a new method which creates a list of holidays in a given month

--- src/main/java/com/vacation/com/vacation/Service/VacationService.java
+++ src/main/java/com/vacation/com/vacation/Service/VacationService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +19,9 @@ public class VacationService {
     public VacationService(HolidayLeaveRepository holidayLeaveRepository) {
         this.holidayLeaveRepository = holidayLeaveRepository;
     }
-
+    public List<HolidayLeave> getVacationsByMonth(int monthNumber) {
+        return holidayLeaveRepository.findByStartDateMonth(monthNumber);
+    }
     public List<HolidayLeave> getAllVacations() {;
         return holidayLeaveRepository.findAll();
     }
