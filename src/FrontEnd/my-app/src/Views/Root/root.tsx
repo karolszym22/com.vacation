f6cdbee601390d0d413da60048ba5246ec88a0ec
Karol Szymański <karol2134@op.pl>
2023-11-21 15:55:02 +0100

    Checks whether the user is logged in. If not, the user can only go to components related to login or registration

--- src/FrontEnd/my-app/src/Views/Root/root.tsx
+++ src/FrontEnd/my-app/src/Views/Root/root.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route,  useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "../../Views/SignIn/SigIn";
-import "../../index.css"
+import "../../index.css";
 import Register from "../../Views/Register/Register";
 import { Provider } from "react-redux";
 import store from "../../Reducers/Store/index";
@@ -9,32 +10,44 @@ import VacationPreview from "../../Components/VacationPreview/VacationPreview";
 import MainTemplate from "../MainTemplate/MainTemplate";
 import Main from "../MainSite/Main";
 import OverlayVisibleProvider from "../../Components/Context/OverlayVisibleContext";
-import Messages from "../Messages/Messages"
+import Messages from "../Messages/Messages";
 import Calendar from "../Calendar/Calendar";
 
-const Root = () => {
- 
+const Root: React.FC = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <OverlayVisibleProvider>
-          <MainTemplate>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/signIn" element={<SignIn />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/newVacation" element={<NewVacation />} />
-              <Route path="/messages" element={<Messages />} />
-              <Route
-                path="/vacationId/:paramValue"
-                element={<VacationPreview />}
-              />
-            </Routes>
-          </MainTemplate>
-        </OverlayVisibleProvider>
+        <App />
       </BrowserRouter>
     </Provider>
+  );
+};
+
+const App: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userString = localStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : null;
+    if (!user && window.location.pathname !== "/register" && window.location.pathname !== "/signIn") {
+      navigate("/signIn");
+    }
+  }, [navigate]);
+
+  return (
+    <OverlayVisibleProvider>
+      <MainTemplate>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/newVacation" element={<NewVacation />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/vacationId/:paramValue" element={<VacationPreview />} />
+        </Routes>
+      </MainTemplate>
+    </OverlayVisibleProvider>
   );
 };
 
