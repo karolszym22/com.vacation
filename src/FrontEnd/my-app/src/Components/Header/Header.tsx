938de8aea7b90aa35220192d0dc275e1a0f7e8ca
Karol Szymański <karol2134@op.pl>
2024-01-08 13:38:30 +0100

    Good practices: moving styled-components down below the generated component

--- src/FrontEnd/my-app/src/Components/Header/Header.tsx
+++ src/FrontEnd/my-app/src/Components/Header/Header.tsx
@@ -1,40 +1,48 @@
 import styled from "styled-components";
 import img from "../../resources/pexels-karolina-grabowska-7876708.jpg";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { FiUser, FiAlignLeft } from "react-icons/fi";
 import { useHeaderData } from "../../Hooks/useHeader";
 
-interface UserState {
-  id: number;
-  name: string;
-  email: string;
-}
+const Header = () => {
+  const {
+    userName,
+    realizedVacations,
+    rejectedVacations,
+    duringVacationCount,
+  } = useHeaderData();
 
-interface Vacation {
-  id: number;
-  daysNum: number;
-  done: boolean;
-  taskStatus: string;
-  startDate: string;
-  endDate: string;
-  employerName: string;
-  personId: number;
-  descritpion: string;
-}
+  return (
+    <HeaderContainer>
+      <HeaderBackground>
+        <HeaderTitle>Dodawaj i obserwuj swoje urlopy! </HeaderTitle>
+        <HeaderElementContainer>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="#2dfc0394">
+              {realizedVacations.length}
+            </HeaderElementValue>
+            <Value>Zaakceptowane</Value>
+          </HeaderElement>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="#f2c121a4">
+              {duringVacationCount}
+            </HeaderElementValue>
+            <Value>W trakcie</Value>
+          </HeaderElement>
+          <HeaderElement>
+            {" "}
+            <HeaderElementValue color="#f3211d93">
+              {rejectedVacations.length}
+            </HeaderElementValue>
+            <Value>Odrzucone</Value>
+          </HeaderElement>
+        </HeaderElementContainer>
+      </HeaderBackground>
+    </HeaderContainer>
+  );
+};
 
-interface AuthorizationState {
-  user: UserState;
-}
-
-interface RootState {
-  authorization: AuthorizationState;
-  vacations: {
-    list: Vacation[];
-    vacationsCount: number;
-  };
-}
+export default Header;
 
 const HeaderContainer = styled.div`
   width: 100%;
@@ -107,7 +115,6 @@ const HeaderElementValue = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
 `;
 
 const HeaderElementContainer = styled.div`
@@ -119,45 +126,3 @@ const HeaderElementContainer = styled.div`
     flex-direction: column;
   }
 `;
-
-const Header = () => {
-  const {
-    userName,
-    realizedVacations,
-    rejectedVacations,
-    duringVacationCount,
-  } = useHeaderData();
-
-  return (
-    <HeaderContainer>
-      <HeaderBackground>
-        <HeaderTitle>Dodawaj i obserwuj swoje urlopy! </HeaderTitle>
-        <HeaderElementContainer>
-          <HeaderElement>
-            {" "}
-            <HeaderElementValue color="#2dfc0394">
-              {realizedVacations.length}
-            </HeaderElementValue>
-            <Value>Zaakceptowane</Value>
-          </HeaderElement>
-          <HeaderElement>
-            {" "}
-            <HeaderElementValue color="#f2c121a4">
-              {duringVacationCount}
-            </HeaderElementValue>
-            <Value>W trakcie</Value>
-          </HeaderElement>
-          <HeaderElement>
-            {" "}
-            <HeaderElementValue color="#f3211d93">
-              {rejectedVacations.length}
-            </HeaderElementValue>
-            <Value>Odrzucone</Value>
-          </HeaderElement>
-        </HeaderElementContainer>
-      </HeaderBackground>
-    </HeaderContainer>
-  );
-};
-
-export default Header;
