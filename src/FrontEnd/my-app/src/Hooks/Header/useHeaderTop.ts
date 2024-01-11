c05502754dc2e21e3f31a693475dc106993d6ebc
Karol Szymański <karol2134@op.pl>
2024-01-11 16:43:06 +0100

    Moving custom hooks to the appropriate folde

---
+++ src/FrontEnd/my-app/src/Hooks/Header/useHeaderTop.ts
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { OverlayVisibleContext } from "../../Components/Context/OverlayVisibleContext";
+import { FiCopy, FiAlignJustify } from "react-icons/fi";
+
+export const useHeaderTopData = () => {
+  const { setOverlayVisible } = useContext(OverlayVisibleContext);
+  const { setHamburgerVisible } = useContext(OverlayVisibleContext);
+
+  const toggleMenu = () => {
+    setOverlayVisible(true);
+    setHamburgerVisible(true);
+  };
+
+  return { toggleMenu };
+};
