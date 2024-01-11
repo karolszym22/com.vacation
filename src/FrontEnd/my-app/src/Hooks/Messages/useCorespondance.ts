d3d8fcf8670ab21e4e627a99ea76be74830b67a6
Karol Szymański <karol2134@op.pl>
2024-01-11 16:35:22 +0100

    Creating an imported hook

---
+++ src/FrontEnd/my-app/src/Hooks/Messages/useCorespondance.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { Correspondences } from "../../Types/Correspondences";
+
+const useCoresspondence = () => {
+  const [allCoresspondences, setAllCoresspondences] = useState<
+    Correspondences[]
+  >([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/correspondences`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAllCoresspondences(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [allCoresspondences]);
+  return {
+    allCoresspondences,
+  };
+};
+
+export default useCoresspondence;
