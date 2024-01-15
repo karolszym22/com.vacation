bbdf52bd8ca7d74ec0ea47c6ea1c4ebf85e7bd94
Karol Szymański <karol2134@op.pl>
2024-01-15 16:19:52 +0100

    Import useCloseOvary hook to hooks folder

---
+++ src/FrontEnd/my-app/src/Hooks/Overlay/useCloseOveraly.ts
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { OverlayVisibleContext } from "../../Components/Context/OverlayVisibleContext";
+
+export const useCloseOverlay = () => {
+    const { setOverlayVisible, setModalVisible } = useContext(OverlayVisibleContext);
+  
+    const closeOverlay = () => {
+      setOverlayVisible(false);
+      setModalVisible(false);
+    };
+  
+    return {
+      closeOverlay,
+      setOverlayVisible,
+      setModalVisible,
+    };
+  };
+  
+
