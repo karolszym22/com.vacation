1246bc6db9e95d131838b898ae95f289b020af0b
Karol Szymański <karol2134@op.pl>
2024-01-16 23:17:51 +0100

    Refactoring

--- src/FrontEnd/my-app/src/Components/VacationPreview/VacationPreview.tsx
+++ src/FrontEnd/my-app/src/Components/VacationPreview/VacationPreview.tsx
@@ -5,18 +5,259 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 import Menu from "../SideMenu/SideMenu";
 import wordIcon from "../../resources/word.png";
-import { Vacation } from "../../Types/Vacation";
-import { RootState } from "../../Types/RootState";
+import { Vacation } from "../../Types/Vacations/Vacation";
+import { RootState } from "../../Types/Vacations/RootState";
 import { FiEdit3 } from "react-icons/fi";
 import HeaderTop from "../Header/HeaderTop";
 import { OverlayVisibleContext } from "../Context/OverlayVisibleContext";
 import Overlay from "../Overlay/Overlay";
+import { downloadDocument } from "../../Utils/downloadDocument";
+import useSendTask from "../../Hooks/VacationPreview/useSendTask";
+import useCheckDocumentExistence from "../../Hooks/VacationPreview/useCheckDocumentExistence";
+import useFetchUserList from "../../Hooks/Messages/useFetchUserList";
+import useFetchVacationData from "../../Hooks/VacationPreview/useFetchVacationData";
 
 interface Overlay {
   overlayVisible: boolean;
   modalVisible: boolean;
   hamburgerVisible: boolean;
 }
+
+
+
+const VacationPreview: React.FC = () => {
+ 
+  const { paramValue } = useParams<{ paramValue: string }>();
+  const userId = useSelector((state: RootState) => state.authorization.user.id);
+  const userName = useSelector(
+    (state: RootState) => state.authorization.user.name
+  );
+  const [, setIsMenuOpen] = useState(false);
+  const { overlayVisible } = useContext(OverlayVisibleContext);
+  const { modalVisible } = useContext(OverlayVisibleContext);
+  const { hamburgerVisible } = useContext(OverlayVisibleContext);
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+
+
+  const userType = useSelector(
+    (state: RootState) => state.authorization.user.employerType
+  );
+
+  const {vacationData} = useFetchVacationData(paramValue)
+   const taskEnums = useSendTask(vacationData, userType)
+  const documentExistence = useCheckDocumentExistence(userId, vacationData);
+
+
+
+  const employerHandleButton = async (taskEnum: string) => {
+    try {
+      let updatedVacationData;
+
+      if (taskEnum === "DO_REALIZACJI") {
+        updatedVacationData = { ...vacationData, taskStatus: "Zrealizowano" };
+
+        const documentData = {
+          description: updatedVacationData?.description,
+          employerName: updatedVacationData?.employerName,
+          daysNum: updatedVacationData?.daysNum,
+          taskStatus: updatedVacationData?.taskStatus,
+          vacationId: updatedVacationData?.id,
+          personId: updatedVacationData?.personId,
+          startDate: updatedVacationData?.startDate,
+          endDate: updatedVacationData?.endDate,
+        };
+
+        const documentResponse = await axios.post(
+          `http://localhost:8080/document/word`,
+          documentData,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      } else if (taskEnum === "ZWROC") {
+        updatedVacationData = { ...vacationData, taskStatus: "Zwrócono" };
+      } else if (taskEnum === "ODRZUC") {
+        updatedVacationData = { ...vacationData, taskStatus: "Odrzucono" };
+      } else if (taskEnum === "ZAAKCEPTUJ") {
+        updatedVacationData = { ...vacationData, taskStatus: "Zaakceptowane" };
+      }
+
+      const response = await axios.put(
+        `http://localhost:8080/vacations/${updatedVacationData?.id}`,
+        updatedVacationData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 204) {
+        console.log("Vacation data updated successfully.");
+      } else {
+        console.error("Failed to update vacation data.");
+      }
+    } catch (error) {
+      console.error("Error handling vacation:", error);
+    }
+  };
+
+  const handleDownloadClick = () => {
+    if(vacationData)
+    {
+      downloadDocument(vacationData, userId);
+    }
+    
+  };
+
+
+  const renderTaskButtons = () => {
+    return taskEnums.map((taskEnum) => {
+      if (taskEnum === "ZAAKCEPTUJ") {
+        return (
+          <TaskButton
+            color="#dcc024d1"
+            key={taskEnum}
+            onClick={() => employerHandleButton(taskEnum)}
+          >
+            {taskEnum}
+          </TaskButton>
+        );
+      }
+      if (taskEnum === "ODRZUC") {
+        return (
+          <TaskButton
+            color="#f3201dd7"
+            key={taskEnum}
+            onClick={() => employerHandleButton(taskEnum)}
+          >
+            {"ODRZUĆ"}
+          </TaskButton>
+        );
+      }
+      if (taskEnum === "ZWROC") {
+        return (
+          <TaskButton
+            color="#cf19bdd6"
+            key={taskEnum}
+            onClick={() => employerHandleButton(taskEnum)}
+          >
+            {"ZWRÓĆ"}
+          </TaskButton>
+        );
+      }
+      if (taskEnum === "DODAJ") {
+        return (
+          <TaskButton
+            color="#e06228d5"
+            key={taskEnum}
+            onClick={() => employerHandleButton(taskEnum)}
+          >
+            {taskEnum}
+          </TaskButton>
+        );
+      }
+      if (taskEnum === "DO_REALIZACJI") {
+        return (
+          <TaskButton
+            color="#4de028d4"
+            key={taskEnum}
+            onClick={() => employerHandleButton(taskEnum)}
+          >
+            {"DO REALIZACJI"}
+          </TaskButton>
+        );
+      }
+    });
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <MainWrapper>
+      <Overlay
+        overlayVisible={overlayVisible}
+        modalVisible={modalVisible}
+        hamburgerVisible={hamburgerVisible}
+        onClose={closeMenu}
+        errorMessage={errorMessage}
+      />
+
+      <Menu></Menu>
+      <PreviewWrapper>
+        <HeaderTop
+          userName={userName}
+          headerText="Szczegółowy podgląd urlopu"
+        />
+        <Header>
+          {" "}
+          <HeaderTitle>Informacje o urlopie</HeaderTitle>
+        </Header>
+
+        {vacationData ? (
+          <WrapperContainer>
+            <VacationDetails>
+              <DetailsContainer>
+                <h2>Szczegóły urlopu</h2>
+                <Text>Pracownik: {vacationData.employerName}</Text>
+                <Text>Ilość dni: {vacationData.daysNum}</Text>
+                <Text>Data rozpoczęcia:</Text>
+                <DateInput
+                  type="date"
+                  value={vacationData.startDate}
+                  readOnly
+                />
+                <Text>Data zakończenia:</Text>
+                <DateInput type="date" value={vacationData.endDate} readOnly />
+                <Text>Opis:</Text>
+                <TextArea rows={4} value={vacationData.description} readOnly />
+              </DetailsContainer>
+              <AdditionalDetailsContainer>
+                <CommentContainer>
+                  <CommentTitle>Komentarz:</CommentTitle>
+                  <CommentContainerArea>
+                    <CommentIcon/>
+                    <CommentArea
+                      readOnly={
+                        userType !== "PRACODAWCA" && userType !== "TESTER"
+                      }
+                    ></CommentArea>
+                  </CommentContainerArea>
+                </CommentContainer>
+                <DocumentDownloadContainer>
+                  <DownloadTitle>Dokumenty:</DownloadTitle>
+                  {documentExistence === "exist" && (
+                    <DocumentDownload onClick={() => handleDownloadClick ()}>
+                    <WordIcon></WordIcon>
+                    Pobierz wniosek
+                  </DocumentDownload>
+                  
+                  )}
+                </DocumentDownloadContainer>
+              </AdditionalDetailsContainer>
+            </VacationDetails>
+
+            <p></p>
+
+            <ButtonContainer>{renderTaskButtons()}</ButtonContainer>
+            <ButtonsContainer></ButtonsContainer>
+          </WrapperContainer>
+        ) : (
+          <p>Brak dokumentu...</p>
+        )}
+      </PreviewWrapper>
+    </MainWrapper>
+  );
+};
+
+export default VacationPreview;
+
 
 const MainWrapper = styled.div`
   display: flex;
@@ -203,320 +444,3 @@ const TextArea = styled.textarea`
     font-size: 12px;
   }
 `;
-
-const VacationPreview: React.FC = () => {
-  const { paramValue } = useParams<{ paramValue: string }>();
-  const [vacationData, setVacationData] = useState<Vacation | null>(null);
-  const [taskEnums, setTaskEnums] = useState<string[]>([]);
-  const userId = useSelector((state: RootState) => state.authorization.user.id);
-  const userName = useSelector(
-    (state: RootState) => state.authorization.user.name
-  );
-  const [documentExistence, setDocumentExistence] = useState<string>("");
-  const [, setIsMenuOpen] = useState(false);
-  const { overlayVisible } = useContext(OverlayVisibleContext);
-  const { modalVisible } = useContext(OverlayVisibleContext);
-  const { hamburgerVisible } = useContext(OverlayVisibleContext);
-  const { setOverlayVisible } = useContext(OverlayVisibleContext);
-  const { setModalVisible } = useContext(OverlayVisibleContext);
-  const [errorMessage, setErrorMessage] = useState("");
-  const userType = useSelector(
-    (state: RootState) => state.authorization.user.employerType
-  );
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8080/vacations/${paramValue}`
-        );
-        const data: Vacation = response.data;
-        setVacationData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [paramValue]);
-
-  const downloadDocument = async () => {
-    try {
-      if (vacationData) {
-        const downloadUrl = `http://localhost:8080/document/word/download`;
-
-        const requestData = {
-          personId: userId,
-          vacationId: vacationData.id,
-        };
-
-        const response = await fetch(downloadUrl, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestData),
-        });
-
-        if (response.ok) {
-          console.log("Document downloaded successfully");
-        } else {
-          console.error("Error downloading document:", response.statusText);
-        }
-      }
-    } catch (error) {
-      console.error("Error downloading document:", error);
-    }
-  };
-
-  useEffect(() => {
-    const sendTask = async () => {
-      try {
-        if (vacationData) {
-          const taskData = {
-            taskStatus: vacationData.taskStatus,
-            userType: userType,
-          };
-          console.log(taskData);
-          const response = await axios.post(
-            `http://localhost:8080/tasksToDo`,
-            taskData,
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-
-          if (response.status === 200) {
-            console.log("Task sent successfully:", response.data.taskEnums);
-            console.log(response.data);
-            setTaskEnums(response.data.taskEnums);
-          } else {
-            console.error("Failed to send task.");
-          }
-        }
-      } catch (error) {
-        console.error("Error sending task:", error);
-      }
-    };
-
-    sendTask();
-  }, [vacationData, userType]);
-
-  const handleTaskButtonClick = (action: string) => {};
-  useEffect(() => {
-    const checkDocumentExistence = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8080/document/word/exist?personId=${userId}&vacationId=${vacationData?.id}`
-        );
-
-        const data: string = response.data;
-        setDocumentExistence(data);
-      } catch (error) {
-        console.error("Error checking document existence:", error);
-      }
-    };
-
-    if (vacationData) {
-      checkDocumentExistence();
-    }
-  }, [vacationData]);
-
-  const employerHandleButton = async (taskEnum: string) => {
-    try {
-      let updatedVacationData;
-
-      if (taskEnum === "DO_REALIZACJI") {
-        updatedVacationData = { ...vacationData, taskStatus: "Zrealizowano" };
-
-        const documentData = {
-          description: updatedVacationData?.description,
-          employerName: updatedVacationData?.employerName,
-          daysNum: updatedVacationData?.daysNum,
-          taskStatus: updatedVacationData?.taskStatus,
-          vacationId: updatedVacationData?.id,
-          personId: updatedVacationData?.personId,
-          startDate: updatedVacationData?.startDate,
-          endDate: updatedVacationData?.endDate,
-        };
-
-        const documentResponse = await axios.post(
-          `http://localhost:8080/document/word`,
-          documentData,
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-      } else if (taskEnum === "ZWROC") {
-        updatedVacationData = { ...vacationData, taskStatus: "Zwrócono" };
-      } else if (taskEnum === "ODRZUC") {
-        updatedVacationData = { ...vacationData, taskStatus: "Odrzucono" };
-      } else if (taskEnum === "ZAAKCEPTUJ") {
-        updatedVacationData = { ...vacationData, taskStatus: "Zaakceptowane" };
-      }
-
-      const response = await axios.put(
-        `http://localhost:8080/vacations/${updatedVacationData?.id}`,
-        updatedVacationData,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (response.status === 204) {
-        console.log("Vacation data updated successfully.");
-      } else {
-        console.error("Failed to update vacation data.");
-      }
-    } catch (error) {
-      console.error("Error handling vacation:", error);
-    }
-  };
-
-  const renderTaskButtons = () => {
-    return taskEnums.map((taskEnum) => {
-      if (taskEnum === "ZAAKCEPTUJ") {
-        return (
-          <TaskButton
-            color="#dcc024d1"
-            key={taskEnum}
-            onClick={() => employerHandleButton(taskEnum)}
-          >
-            {taskEnum}
-          </TaskButton>
-        );
-      }
-      if (taskEnum === "ODRZUC") {
-        return (
-          <TaskButton
-            color="#f3201dd7"
-            key={taskEnum}
-            onClick={() => employerHandleButton(taskEnum)}
-          >
-            {"ODRZUĆ"}
-          </TaskButton>
-        );
-      }
-      if (taskEnum === "ZWROC") {
-        return (
-          <TaskButton
-            color="#cf19bdd6"
-            key={taskEnum}
-            onClick={() => employerHandleButton(taskEnum)}
-          >
-            {"ZWRÓĆ"}
-          </TaskButton>
-        );
-      }
-      if (taskEnum === "DODAJ") {
-        return (
-          <TaskButton
-            color="#e06228d5"
-            key={taskEnum}
-            onClick={() => employerHandleButton(taskEnum)}
-          >
-            {taskEnum}
-          </TaskButton>
-        );
-      }
-      if (taskEnum === "DO_REALIZACJI") {
-        return (
-          <TaskButton
-            color="#4de028d4"
-            key={taskEnum}
-            onClick={() => employerHandleButton(taskEnum)}
-          >
-            {"DO REALIZACJI"}
-          </TaskButton>
-        );
-      }
-    });
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
-  return (
-    <MainWrapper>
-      <Overlay
-        overlayVisible={overlayVisible}
-        modalVisible={modalVisible}
-        hamburgerVisible={hamburgerVisible}
-        onClose={closeMenu}
-        errorMessage={errorMessage}
-      />
-
-      <Menu></Menu>
-      <PreviewWrapper>
-        <HeaderTop
-          userName={userName}
-          headerText="Szczegółowy podgląd urlopu"
-        />
-        <Header>
-          {" "}
-          <HeaderTitle>Informacje o urlopie</HeaderTitle>
-        </Header>
-
-        {vacationData ? (
-          <WrapperContainer>
-            <VacationDetails>
-              <DetailsContainer>
-                <h2>Szczegóły urlopu</h2>
-                <Text>Pracownik: {vacationData.employerName}</Text>
-                <Text>Ilość dni: {vacationData.daysNum}</Text>
-                <Text>Data rozpoczęcia:</Text>
-                <DateInput
-                  type="date"
-                  value={vacationData.startDate}
-                  readOnly
-                />
-                <Text>Data zakończenia:</Text>
-                <DateInput type="date" value={vacationData.endDate} readOnly />
-                <Text>Opis:</Text>
-                <TextArea rows={4} value={vacationData.description} readOnly />
-              </DetailsContainer>
-              <AdditionalDetailsContainer>
-                <CommentContainer>
-                  <CommentTitle>Komentarz:</CommentTitle>
-                  <CommentContainerArea>
-                    <CommentIcon></CommentIcon>
-                    <CommentArea
-                      readOnly={
-                        userType !== "PRACODAWCA" && userType !== "TESTER"
-                      }
-                    ></CommentArea>
-                  </CommentContainerArea>
-                </CommentContainer>
-                <DocumentDownloadContainer>
-                  <DownloadTitle>Dokumenty:</DownloadTitle>
-                  {documentExistence === "exist" && (
-                    <DocumentDownload onClick={downloadDocument}>
-                      <WordIcon></WordIcon>
-                      Pobierz wniosek
-                    </DocumentDownload>
-                  )}
-                </DocumentDownloadContainer>
-              </AdditionalDetailsContainer>
-            </VacationDetails>
-
-            <p></p>
-
-            <ButtonContainer>{renderTaskButtons()}</ButtonContainer>
-            <ButtonsContainer></ButtonsContainer>
-          </WrapperContainer>
-        ) : (
-          <p>Brak dokumentu...</p>
-        )}
-      </PreviewWrapper>
-    </MainWrapper>
-  );
-};
-
-export default VacationPreview;
